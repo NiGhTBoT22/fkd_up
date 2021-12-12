7e59771b88c299ff25a25730c9ff754e938c70c1
NiGhT_BoT_22 <88623043+NiGhTBoT22@users.noreply.github.com>
2021-12-12 19:26:15 +0530

    Add files via upload

---
+++ mob_app/data.py
@@ -0,0 +1,7 @@
+question_data = [
+    {"text":"what is CPU ? a.)central processing unit b.)central processor unit c.)central print unit","answer":"a"},
+    {"text":"what is GPU ? a.)graphics processor unit b.)graphics processing unit c.)graphics print unit","answer":"b"},
+    {"text":"what is RAM ? a.)random access memory b.)read only memory c.) random accessed memory","answer":"a"},
+    {"text":"what is ROM ? a.)random only memory b.)read once memory c.)read only memory","answer":"c"},
+    {"text":"what is V-RAM ? a.)video-RAM b.)virtual-RAM c.)VFX-RAM","answer":"b"}
+]
