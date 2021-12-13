39df72ba186ab0da60c1ca885d9cd5b75d48c7e4
NiGhT_BoT_22 <88623043+NiGhTBoT22@users.noreply.github.com>
2021-12-13 21:26:08 +0530

    Update data.py

--- mob_app/data.py
+++ mob_app/data.py
@@ -3,5 +3,8 @@ question_data = [
     {"text":"what is GPU ? a.)graphics processor unit b.)graphics processing unit c.)graphics print unit","answer":"b"},
     {"text":"what is RAM ? a.)random access memory b.)read only memory c.) random accessed memory","answer":"a"},
     {"text":"what is ROM ? a.)random only memory b.)read once memory c.)read only memory","answer":"c"},
-    {"text":"what is V-RAM ? a.)video-RAM b.)virtual-RAM c.)VFX-RAM","answer":"b"}
+    {"text":"what is V-RAM ? a.)video-RAM b.)virtual-RAM c.)VFX-RAM","answer":"b"}, 
+    {"text":"what is PS4 ? a.)play station b.)power supply c.)power station","answer":"a"},
+    {"text":"what is PSU ? a.)play station unit b.)power supply unit c.)power station unit","answer":"b"},
+    {"text":"what is SIM ? a.)subscriber index module b.)satellite index module c.)seller index module:"a"} 
 ]
